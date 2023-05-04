1544da25f5289c65b54fdeef5e0f4b975afecbc1
Almataeva Anastasia <linx321@yandex.ru>
2023-05-04 23:27:01 +0300

    fix: login and package.json

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
 // компонент авторизации пользователя
 export default function Login({ children }) {
-  return <div className="login">{children}</div>;
+  return <div className="login">{children}<div className="login__signin"/></div>;
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -3,7 +3,7 @@ export default function Register({ children }) {
   return (
     <div className="register">
       {children}
-      <div className="register__signin">
+      <div className="register__signup">
         <p className="register__text">Уже зарегистрированы?</p>
         <a href="/sign-in" className="register__login-link link">
           Войти
